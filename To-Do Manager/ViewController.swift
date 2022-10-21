826e88a21c5c7cf7cb415dbac49ff25a62b1d92f
DMYTRO <dmytro.neboha@proton.me>
2022-10-21 19:37:07 +0300

    Creation model prototype

--- To-Do Manager/ViewController.swift
+++ To-Do Manager/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  To-Do Manager
-//
-//  Created by Dmytro Neboha on 21.10.2022.
-//
+
 
 import UIKit
 
@@ -11,7 +6,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
